787af43a5822e239684e8d5481f95f89ac3030d2
Stefan Binder <binder.stefan@hispeed.ch>
2022-07-02 16:27:25 +0200

    Add innoverio.vscode-dbt-power-user

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -7,6 +7,7 @@ eamodio.gitlens
 alefragnani.project-manager
 vscode-icons-team.vscode-icons
 ms-vscode-remote.vscode-remote-extensionpack
+innoverio.vscode-dbt-power-user
 )
 
 for i in ${packagelist[@]}; do
